732cdb0182a204b3faad49287885301a4ccce68c
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2020-05-05 03:16:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import quiz
 downloader.first_run()
 listen = threading.Thread(target=listener.listen(), daemon=True)
 update = threading.Thread(target=downloader.update(), daemon=True)
-quiz = threading.Thread(target=quiz.quizer(), daemon=True)
 
 update.start()
 listen.start()
-quiz.start()
