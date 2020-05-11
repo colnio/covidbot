3f44a4e066205f1f2e10ccdab00cf05ef4e56108
Co1IO <yra200111ruz@gmail.com>
2020-05-11 22:24:32 +0300

    Fixed threads' names

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import listener
 import downloader
 
 downloader.first_run()
-listen = threading.Thread(target=listener.listen(), daemon=True)
-update = threading.Thread(target=downloader.update(), daemon=True)
+listen = threading.Thread(target=listener.listen(), daemon=True, name='listen')
+update = threading.Thread(target=downloader.update(), daemon=True, name='update')
 
 update.start()
 listen.start()
